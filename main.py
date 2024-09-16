46b2924ab36acbcdc9dfd1e8098d89174cd246b5
Péter Szedmák <thazulk@gmail.com>
2024-09-16 12:03:21 +0200

    working black screen yaay

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    BLACK = (0, 0, 0)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(BLACK)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
