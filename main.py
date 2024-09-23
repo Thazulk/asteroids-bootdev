9bb4db8a58f9b2a3fbc97d0adce618c291ff25ec
Péter Szedmák <thazulk@gmail.com>
2024-09-23 21:44:19 +0200

    basic player class and polygon drawer added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,8 +10,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     BLACK = (0, 0, 0)
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     running = True
     while running:
@@ -17,7 +25,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill(BLACK)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
